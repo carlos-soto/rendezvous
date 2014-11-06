2813254f4e332ba8d33a8e9e61646ece16cefefa
Carlos Soto <carlos.soto@lognllc.com>
2014-11-06 12:17:53 -0600

    Adding Empty Yeoman FE App
    Updated Angular to 1.3.0

---
+++ fe/app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name rendezvousApp
+ * @description
+ * # rendezvousApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('rendezvousApp', []);
